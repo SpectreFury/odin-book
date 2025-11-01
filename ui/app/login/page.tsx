7fce7a02c841e9658a03ed9a1c73ea9d303fbd01
Ayush Soni <soni.ayush.2212@gmail.com>
2025-11-01 22:03:30 +0530

    feat: add login page

---
+++ ui/app/login/page.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { useForm, Controller } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const formSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(5).max(65),
+});
+
+const Login = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof formSchema>) {
+    console.log(data.email, data.password);
+  }
+
+  return (
+    <main className="flex justify-center">
+      <div className="flex flex-col justify-center container items-center mt-20">
+        <h1 className="text-2xl font-medium">Login</h1>
+        <div className="w-[600px]">
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FieldGroup>
+              <Controller
+                name="email"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field aria-invalid={fieldState.invalid}>
+                    <FieldLabel htmlFor="email">Email</FieldLabel>
+                    <Input
+                      {...field}
+                      id="email"
+                      aria-invalid={fieldState.invalid}
+                      placeholder={"Login"}
+                      autoComplete="off"
+                    />
+                  </Field>
+                )}
+              />
+              <Controller
+                name="password"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field aria-invalid={fieldState.invalid}>
+                    <FieldLabel htmlFor="email">Password</FieldLabel>
+                    <Input
+                      {...field}
+                      id="email"
+                      aria-invalid={fieldState.invalid}
+                      placeholder={"Password"}
+                      autoComplete="off"
+                    />
+                  </Field>
+                )}
+              />
+            </FieldGroup>
+
+            <Field orientation="horizontal" className="mt-2">
+              <Button type="submit">Login</Button>
+            </Field>
+          </form>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Login;
